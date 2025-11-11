0c1edd2ecce19484a8869cf4f0882d3b7d0eec6b
rindraniaina <rindraniainarasolofondrasoa17@gmail.com>
2025-11-11 21:57:21 +0300

    feat: 2. afficher les employes qui n'ont jamais pris de conge

--- evaluation.sql
+++ evaluation.sql
@@ -4,4 +4,7 @@ select Employee.id, Employee.first_name, Employee.last_name from Employee
   left join Team on Employee.id = Team.id_employee
   where Team.id_employee is null;
 
+--2
 
+select Employee.id, Employee.first_name, Employee.last_name from Employee
+  where id_leave is null;
